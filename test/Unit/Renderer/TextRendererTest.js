7d09b448c44c007890ef8449a046df50f4bad476
Marc Breitung <info@marcbreitung.de>
2018-03-06 18:59:13 +0100

    Update node_modules BUG: TextRenderer with empty label caused wrong output

--- test/Unit/Renderer/TextRendererTest.js
+++ test/Unit/Renderer/TextRendererTest.js
@@ -18,9 +18,11 @@ suite('TextRenderer', function () {
 
             let layer = new Layer('layer', {
                 'nodes': [{
+                    'label': 'A',
                     'position': {'x': 10, 'y': 10},
                     'childs': [{'position': {'x': 40, 'y': 40}}]
                 }, {
+                    'label': 'B',
                     'position': {'x': 10, 'y': 10},
                     'childs': [{'position': {'x': 40, 'y': 40}}]
                 }],
@@ -30,12 +32,44 @@ suite('TextRenderer', function () {
 
             let textRenderer = new TextRenderer();
 
+            let spyDrawCircle = sinon.spy(textRenderer, 'drawCircle');
             let spyDrawText = sinon.spy(textRenderer, 'drawText');
 
             textRenderer.renderLayer(context, layer);
 
+            assert.isTrue(spyDrawCircle.calledTwice);
             assert.isTrue(spyDrawText.calledTwice);
         });
+
+        test('should not render text if the label is empty', function () {
+            let dom = (new JSDOM(`<body><canvas id="search-map"></canvas></body>`));
+            let searchMap = dom.window.document.getElementById('search-map');
+            let context = searchMap.getContext('2d');
+
+            let layer = new Layer('layer', {
+                'nodes': [{
+                    'position': {'x': 10, 'y': 10},
+                    'childs': [{'position': {'x': 40, 'y': 40}}]
+                }, {
+                    'label': '',
+                    'position': {'x': 10, 'y': 10},
+                    'childs': [{'position': {'x': 40, 'y': 40}}]
+                }],
+                'nodeColor': '#e34f00',
+                'lineColor': '#385171'
+            });
+
+            let textRenderer = new TextRenderer();
+
+            let spyDrawCircle = sinon.spy(textRenderer, 'drawCircle');
+            let spyDrawText = sinon.spy(textRenderer, 'drawText');
+
+            textRenderer.renderLayer(context, layer);
+
+            assert.isTrue(spyDrawCircle.calledTwice);
+            assert.isTrue(spyDrawText.notCalled);
+        });
+
     });
 
 });
